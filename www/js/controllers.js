5d4a3eb1d909e72f7a052a51128d52f0efa3019f
Imee Cuison <imee@dynepic.com>
2016-04-23 10:29:16 -0400

    camera enabled

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,34 +1,74 @@
 angular.module('app.controllers', [])
-  
+
 .controller('homeTabDefaultPageCtrl', function($scope) {
 
 })
-   
-.controller('inputTabDefaultPageCtrl', function($scope) {
+
+.controller('inputTabDefaultPageCtrl', function($scope, $cordovaCamera) {
+
+    $scope.takePhoto = function () {
+        var options = {
+          quality: 75,
+          destinationType: Camera.DestinationType.DATA_URL,
+          sourceType: Camera.PictureSourceType.CAMERA,
+          allowEdit: true,
+          encodingType: Camera.EncodingType.JPEG,
+          targetWidth: 300,
+          targetHeight: 300,
+          popoverOptions: CameraPopoverOptions,
+          saveToPhotoAlbum: false
+      };
+
+          $cordovaCamera.getPicture(options).then(function (imageData) {
+              $scope.imgURI = "data:image/jpeg;base64," + imageData;
+          }, function (err) {
+              // An error occured. Show a message to the user
+          });
+      }
+
+      $scope.choosePhoto = function () {
+         var options = {
+           quality: 75,
+           destinationType: Camera.DestinationType.DATA_URL,
+           sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+           allowEdit: true,
+           encodingType: Camera.EncodingType.JPEG,
+           targetWidth: 300,
+           targetHeight: 300,
+           popoverOptions: CameraPopoverOptions,
+           saveToPhotoAlbum: false
+       };
+
+           $cordovaCamera.getPicture(options).then(function (imageData) {
+               $scope.imgURI = "data:image/jpeg;base64," + imageData;
+           }, function (err) {
+               // An error occured. Show a message to the user
+           });
+       }
+
 
 })
-   
+
 .controller('statsTabDefaultPageCtrl', function($scope) {
 
 })
-      
+
 .controller('mapDefaultPageCtrl', function($scope) {
 
 })
-   
+
 .controller('loginCtrl', function($scope) {
 
 })
-   
+
 .controller('signupCtrl', function($scope) {
 
 })
-   
+
 .controller('myRecentActivityCtrl', function($scope) {
 
 })
-   
+
 .controller('myStatsCtrl', function($scope) {
 
 })
- 
